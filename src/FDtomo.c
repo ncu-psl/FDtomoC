290d2d254663e730f59a154bc25244b8d6bc3da0
q1qip123 <q1qip123@gmail.com>
2019-10-06 16:32:04 +0800

    add CMakeLists txts and FDtomo.c

---
+++ src/FDtomo.c
@@ -0,0 +1,16 @@
+//#include "FDtomo/make1d.h"
+#include <stdio.h>
+int main(int argc, char *argv[]){
+	char *s;
+	make1d(argv[1]);
+	//c2f(argv[1]);
+	//scanf("%s", s);
+	//sphfd(argv[1]);
+
+	//sphfdloc(argv[1]);
+	//sphrayderv(argv[1]);
+	//runlsqr(argv[1]);
+	//makenewmod(argv[1]);
+	return 0;
+
+}
