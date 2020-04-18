9d75b1372c75ebded8196105e321431ff9556ec5
q1qip123 <q1qip123@gmail.com>
2020-04-18 09:54:39 +0000

    addTypeStation

---
+++ include/common/station.h
@@ -0,0 +1,14 @@
+#ifndef STATION
+#define STATION
+#include "common/grid.h"
+struct station{
+    char *name;
+    Point3D location;
+    struct station *next;
+};
+typedef struct station Station;
+
+Station *createNewStation(char *, Point3D);
+void addStation(Station *, Station *);
+Station *createStations(char *file, int sph);
+#endif
