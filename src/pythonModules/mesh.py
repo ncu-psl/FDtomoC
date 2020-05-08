14c33f0c226b04397358c601a62a4e14f7204c7d
q1qip123 <q1qip123@gmail.com>
2020-05-08 11:05:10 +0800

    add methods of Mesh

--- src/pythonModules/mesh.py
+++ src/pythonModules/mesh.py
@@ -6,6 +6,11 @@ class Mesh(object):
     def __init__(self, numberOfNode = None, igrid = None):
         self.numberOfNode = numberOfNode
         self.igrid = igrid
+        self.meshField = None
+
+    @abc.abstractmethod
+    def create(self):
+        return NotImplemented
 
     @abc.abstractmethod
     def getSize(self):
@@ -21,6 +26,12 @@ class Mesh(object):
 
 
 class Mesh1D(Mesh):
+    def create(self, file = None, numberOfNode = None, igrid = None):
+        mesh = Mesh1D(numberOfNode, igrid)
+        tmp = _FDtomoC.ffi.new("int[]", igrid)
+        mesh.meshField = _FDtomoC.lib.createMesh1D(numberOfNode, tmp)
+        return mesh
+
     def getSize(self):
         return self.numberOfNode
 
@@ -28,15 +39,28 @@ class Mesh1D(Mesh):
         mesh = _FDtomoC.ffi.new("Mesh1D *", {'numberOfNode' : self.numberOfNode})
         igrid = _FDtomoC.ffi.new("int[]", self.igrid)
         mesh.igrid = igrid
-        return mesh
+        self.mesh = mesh
 
 
 class Mesh3D(Mesh):
+    def create(self, file = None, numberOfNode = None, igrid = None):
+        if (file != None):
+            mesh = Mesh3D()
+            tmp = _FDtomoC.ffi.new("char[]", file)
+            mesh.meshField = _FDtomoC.lib.setMesh3D(tmp)
+            return mesh
+
+
     def getSize(self):
         size = 0
         for i in self.numberOfNode:
             size = size + i
         return size
+
+    def generateFineMesh(self):
+        mesh = Mesh3D()
+        mesh.meshField = _FDtomoC.lib.generateFineMesh(self.meshField)
+        return mesh
 
     def translate(self):
         gridx = _FDtomoC.ffi.new("int[]", self.igrid[0])
@@ -45,4 +69,4 @@ class Mesh3D(Mesh):
         mesh = _FDtomoC.ffi.new("Mesh3D *", {'numberOfNode' : self.numberOfNode, 'gridx' : gridx, \
                                                 'gridy' : gridy, 'gridz' : gridz})
 
-        return mesh
+        self.mesh = mesh 
